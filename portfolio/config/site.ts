375fd66093f253aec52101a5fd23d8709052c24a
justinp04 <justin.pan688@gmail.com>
2025-02-08 12:48:06 +0800

    Navbar updated

---
+++ portfolio/config/site.ts
@@ -0,0 +1,32 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+    name: "Next.js + HeroUI",
+    description:
+        "Make beautiful websites regardless of your design experience.",
+    navItems: [
+        {
+            label: "About",
+            href: "/",
+        },
+        {
+            label: "Experience",
+            href: "/docs",
+        },
+        {
+            label: "Projects",
+            href: "/pricing",
+        },
+        {
+            label: "Contact",
+            href: "/blog",
+        },
+    ],
+    links: {
+        github: "https://github.com/heroui-inc/heroui",
+        twitter: "https://twitter.com/hero_ui",
+        docs: "https://heroui.com",
+        discord: "https://discord.gg/9b6yyZKmH4",
+        sponsor: "https://patreon.com/jrgarciadev",
+    },
+};
